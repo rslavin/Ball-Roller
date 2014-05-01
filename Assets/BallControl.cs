939186729179f934d49c6056295b6b80009d567c
U-Rocky-PC\Rocky <Rocky@Rocky-PC.(none)>
2014-04-30 22:23:18 -0500

    Ball rolls with a and d keys

---
+++ Assets/BallControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControl : MonoBehaviour {
+	float rotationSpeed = 100;
+
+	// Use this for initialization
+	void Start () {
+		// called when the game starts
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// called every time a frame is rendered
+
+		float rotation = Input.GetAxis ("Horizontal") * rotationSpeed;
+
+		// make sure the ball rotates at the same speed independent of framerate
+		rotation *= Time.deltaTime;
+
+		//make the object rotate
+		// Vector3 (3d coords) in direction "back"
+		rigidbody.AddRelativeTorque (Vector3.back * rotation);
+
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# This is my first attempt at using Unity to create a game
