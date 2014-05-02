40fe741c135d98b248595cfe2b14000543ec04dc
Rocky Slavin <rocky.slavin@gmail.com>
2014-05-01 20:28:13 -0500

    added coin that despawns when picked up

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinPickup : MonoBehaviour {
+
+	// Called when something enters it
+	void OnTriggerEnter (Collider info) {
+		// info will be the thing that enters the coin
+	
+		// make sure to tag the ball as player
+		if (info.tag.Equals ("Player")) {
+			//Debug.Log ("coin picked up");
+
+			// gameObject is the object the script is attached to
+			Destroy (gameObject);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -13,5 +13,9 @@ Level resets when ball falls
 
 
 notes:
-Textures from cgtextures.com
-Debug.Log(String) is a console printer for debugging
+-Textures from cgtextures.com
+-Debug.Log(String) is a console printer for debugging
+-Edit>Project Settings>Quality to change quality levels for 
+whatever platform it's running on.
+-Use "Is Trigger" in Collider to make things go through it and
+use it as a trigger for something instead.
